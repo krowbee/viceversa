651ab6ca85b8d0576555a1f19b4fefb33e1295b6
krowbee <skoresplay@gmail.com>
2023-09-07 13:44:57 +0300

    Add word counter

--- ViceVersa/ViceVersa/views.py
+++ ViceVersa/ViceVersa/views.py
@@ -10,4 +10,6 @@ def home(request):
 def reverse(request):
     user_text = request.GET['usertext']
     reversed_text = user_text [::-1]
-    return render(request,"reverse.html",{'user_text':user_text,'reversed_text':reversed_text})
+    words = user_text.split()
+    num_words = len(words)
+    return render(request,"reverse.html",{'user_text':user_text,'reversed_text':reversed_text,'num_words':num_words})
